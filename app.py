44ba767cee27f0f4a6fae2ead5cf4b90a847cfe8
ntungufhadzeni <ntungufhadzenimbudzeno@gmail.com>
2022-05-10 10:54:38 +0200

    add server

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+server = app.server
 
 
 @app.route("/sms", methods=['GET', 'POST'])
